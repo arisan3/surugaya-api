8faf8cdb62b7683fd9b144ff188c7e3b18f1295d
arisan3 <a.arisan3@gmail.com>
2025-07-31 13:37:18 +0900

    7/31-7Update main.py

--- main.py
+++ main.py
@@ -2,74 +2,88 @@ import gspread
 from google.oauth2.service_account import Credentials
 import requests
 from bs4 import BeautifulSoup
-import time
 import re
+import time
 
-# Googleサービスアカウントの設定
-JSON_PATH = 'totemic-creek-306512-640467f70542.json'
+# 認証
+JSON_PATH = 'your_service_account.json'  # ←あなたのJSONパスに修正
 SCOPES = ['https://www.googleapis.com/auth/spreadsheets']
 creds = Credentials.from_service_account_file(JSON_PATH, scopes=SCOPES)
 gc = gspread.authorize(creds)
 
+# スプレッドシート設定
 SPREADSHEET_ID = '1QtWsWnKMdC9882a-_9XWDsGyp4b-LPyK0aVZDlvcpfs'
 SHEET_NAME = '駿河屋リサーチ'
 ws = gc.open_by_key(SPREADSHEET_ID).worksheet(SHEET_NAME)
 
-def search_surugaya_products(keyword, max_count=20):
+# A列のキーワードからB列に商品タイトル（日本語） D列に販売ページURL
+keywords = ws.col_values(1)[4:]  # A5:A
+
+# 1商品最大20件取得
+MAX = 20
+
+def get_surugaya_items(keyword):
     url = f'https://www.suruga-ya.jp/search?category=&search_word={requests.utils.quote(keyword)}&searchbox=1'
     res = requests.get(url, timeout=10)
     soup = BeautifulSoup(res.content, 'html.parser')
     items = []
-    for a in soup.select('.item_detail .title > a')[:max_count]:
+    for a in soup.select('.item_detail a[href*="/product/"]')[:MAX]:
         title = a.get_text(strip=True)
-        href = a.get('href')
-        if not href.startswith('http'):
-            href = 'https://www.suruga-ya.jp' + href
-        items.append((title, href))
+        link = 'https://www.suruga-ya.jp' + a['href']
+        items.append({'title': title, 'url': link})
     return items
 
-def get_jan_code(product_url):
-    try:
-        res = requests.get(product_url, timeout=10)
-        soup = BeautifulSoup(res.content, 'html.parser')
-        # JANコードは商品詳細テーブル内に出現
-        text = soup.get_text()
-        m = re.search(r'\b\d{13}\b', text)
-        if m:
-            return m.group(0)
-        return ''
-    except Exception as e:
-        return ''
+def get_english_title(jp_title):
+    # ここはDeepL APIやGoogle翻訳APIと連携可能（ここではダミー）
+    return jp_title  # 英訳が不要ならそのまま返す
 
-# 検索キーワード取得（A列5行目以降）
-keywords = ws.col_values(1)[4:]
+def get_jan_from_kaitori(title):
+    # 駿河屋買取ページで検索してJAN取得
+    search_url = f'https://www.suruga-ya.jp/kaitori/search_buy?category=&search_word={requests.utils.quote(title)}&searchbox=1'
+    res = requests.get(search_url, timeout=10)
+    soup = BeautifulSoup(res.content, 'html.parser')
+    a = soup.select_one('div.item_detail a[href*="/product/other/"]')
+    if not a:
+        return ''
+    detail_url = 'https://www.suruga-ya.jp' + a['href']
+    detail_res = requests.get(detail_url, timeout=10)
+    detail_soup = BeautifulSoup(detail_res.content, 'html.parser')
+    m = re.search(r'\b\d{13}\b', detail_soup.get_text())
+    if m:
+        return m.group(0)
+    return ''
+
 all_titles = []
+all_titles_en = []
 all_urls = []
 all_jans = []
 
-for keyword in keywords:
-    if not keyword.strip():
-        continue
-    print(f'キーワード「{keyword}」検索中…')
-    results = search_surugaya_products(keyword, max_count=20)
-    if not results:
+for kw in keywords:
+    items = get_surugaya_items(kw)
+    if not items:
+        # No hit
         all_titles.append(['該当商品なし'])
+        all_titles_en.append(['No matching products'])
         all_urls.append([''])
         all_jans.append([''])
         continue
-    for title, url in results:
-        all_titles.append([title])
-        all_urls.append([url])    # URLのまま
-        jan = get_jan_code(url)
+    for i, item in enumerate(items):
+        all_titles.append([item['title']])
+        all_titles_en.append([get_english_title(item['title'])])
+        # D列：URLを直接貼り付け（スプレッドシート側で自動でリンク化される）
+        all_urls.append([item['url']])
+        # E列：JANコード（買取ページから取得）
+        jan = get_jan_from_kaitori(item['title'])
         all_jans.append([jan])
-        time.sleep(1.0)  # 負荷対策
+        print(f"{kw} {i+1}件目: {item['title']} - {jan}")
+        time.sleep(1.2)
 
-# スプレッドシートへ一括書き込み
+# スプレッドシートに一括書き込み
 if all_titles:
-    ws.update('B5:B{}'.format(4+len(all_titles)), all_titles)
-if all_urls:
-    ws.update('D5:D{}'.format(4+len(all_urls)), all_urls)
-if all_jans:
-    ws.update('E5:E{}'.format(4+len(all_jans)), all_jans)
-
-print('全て完了！')
+    ws.update(all_titles, range_name=f'B5:B{4+len(all_titles)}')
+    ws.update(all_titles_en, range_name=f'C5:C{4+len(all_titles_en)}')
+    ws.update(all_urls, range_name=f'D5:D{4+len(all_urls)}')
+    ws.update(all_jans, range_name=f'E5:E{4+len(all_jans)}')
+    print("書き込み完了！")
+else:
+    print("対象データなし")
